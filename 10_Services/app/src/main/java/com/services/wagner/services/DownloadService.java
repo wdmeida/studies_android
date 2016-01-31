fcade5a68da709c53f346b47c808fa8c5a8081ff
Wagner Almeida <wdmeida@gmail.com>
2016-01-31 18:40:56 -0200

    Atualização

--- 10_Services/app/src/main/java/com/services/wagner/services/DownloadService.java
+++ 10_Services/app/src/main/java/com/services/wagner/services/DownloadService.java
@@ -17,17 +17,57 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-/**
- * Created by Wagner on 31/01/2016.
+/*
+ *  Para criar um serviço é preciso implementar uma extensão da classe Service e
+ *  sobrescrever alguns métodos de callback.
+ *
+ *  onStartCommand() - Método que inicia um serviço indefinidamente. O serviço apenas
+ *  termina quando o método stopSelf() é executado a partir do próprio serviço ou quando
+ *  o método stopService() é executado a partir de outra aplicação. O método devolve um inteiro.
+ *  Este valor indica como o sistema deve continuar o serviço caso o sistema o mate.
+ *  Existem 3 valores possíveis:
+ *
+ *      START_NOT_STICKY - Não reinicia o serviço a menos que haja Intents a serem entregues.
+ *
+ *      START_STICKY - Reinicia o serviço mas não continua a partir do Intent que estava em
+ *      execução mas apenas para os que estavam pendentes;
+ *
+ *      START_REDELIVER_INTENT - Reinicia o serviço retornando a partir do Intent que estava
+ *      em execução.
+ *
+ *  onBind() - Método que é chamado pelo sistema para associar o serviço a uma aplicação.
+ *  Ele deve prover uma interface de comunicação entre ambos. Este método deve ser implementado
+ *  obrigatóriamente, logo, se o serviço não for desenhado para suportar Bind então o método
+ *  onBind() deve devolver null.
+ *
+ *  onCreate() - Método chamado pelo sistema no momento da criação do serviço e pode ser
+ *  utilizado para realizar pré configurações.
+ *
+ *  onDestroy() - Método chamado pelo sistema quando o serviço for destruído e pode ser utilizado
+ *  para liberar recursos utilizados.
+ *
+ *  É muito comum implementar serviços que utilizem sua própria thread para executar as tarefas
+ *  requisitadas, desta forma, o framework fornece uma extensão da classe Service que simplifica a
+ *  criação de serviçoes como mostrado na classe abaixo. O código abaixo implementa um serviço através
+ *  da classe IntentService.
  */
 public class DownloadService extends IntentService {
 
     private int result = Activity.RESULT_CANCELED;
 
+    /*
+        O construtor é obrigatório e deve chamar o contrutor da super classe
+        passando o nome da Thread worker.
+     */
     public DownloadService() {
         super("DownloadService");
     }
 
+    /*
+        Este método é chamado pela IntentService a partir de um worker Thread e recebe o
+        Intent que iniciou o serviço. Quando o método termina o IntentService para o serviço.
+        Quando se utiliza a classe IntentService, não é necessário se preocupar em parar o serviço.
+     */
     @Override
     protected void onHandleIntent(Intent intent){
         Uri data = intent.getData();
@@ -40,12 +80,13 @@ public class DownloadService extends IntentService {
 
         InputStream stream = null;
         FileOutputStream fos = null;
+
         try {
             URL url = new URL(urlPath);
             stream = url.openConnection().getInputStream();
             InputStreamReader reader = new InputStreamReader(stream);
             fos = new FileOutputStream(output.getPath());
-            int next = -1;
+            int next;
             while ((next = reader.read()) != -1){
                 fos.write(next);
             }
