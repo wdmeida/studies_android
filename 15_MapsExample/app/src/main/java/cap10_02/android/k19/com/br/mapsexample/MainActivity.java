9de2879480dc53e1127124b92923043511a8e530
Wagner Almeida <wdmeida@gmail.com>
2016-07-05 10:34:29 -0300

    Correção da Activity principal.

--- 15_MapsExample/app/src/main/java/cap10_02/android/k19/com/br/mapsexample/MainActivity.java
+++ 15_MapsExample/app/src/main/java/cap10_02/android/k19/com/br/mapsexample/MainActivity.java
@@ -2,10 +2,19 @@ package cap10_02.android.k19.com.br.mapsexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
+/*
+    Para implementar a visualização do mapa da api do google maps, é necessário que a classe implemente
+    a classe OnMapReadyCallback. É necessário declarar no build.gradle o compile 'com.google.android.gms:play-services:9.2.0',
+    para que a api possa ser referênciada sem erros. Não esquecer também de definir do AndroidManifest.xml as seguintes permissões:
+            <uses-permission android:name="android.permission.INTERNET" />
+            <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+            <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+ */
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
@@ -19,11 +28,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(GoogleMap map) throws SecurityException {
+    public void onMapReady(GoogleMap map) {
         LatLng sydney = new LatLng(-33.867, 151.206);
 
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+        try {
+            map.setMyLocationEnabled(true);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+        } catch (SecurityException e) {
+            Log.d("Erro de localização: ", e.getMessage());
+        }
 
         map.addMarker(new MarkerOptions()
                 .title("Sydney")
